192feff571237a4160c347d0cd165849b60f3ef8
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-08-20 14:54:51 -0600

    links are working mvp met

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -11,8 +14,14 @@ const App = () => {
 
   return (
     <div>
+      <Router>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" render={props => <MovieList {...props} movies={MovieList} />} />
+      <Route path="/movies/:id" render={props => <Movie {...props} movie={Movie} />} />
+      </Router>  
+
+      
+
     </div>
   );
 };
